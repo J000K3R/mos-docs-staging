30ba1ebf6707cac024b341979e85f37b3bf4c2f7
J000K3R <nico.nestler@evil-corp.at>
2026-03-16 16:15:57 +0000

    Fix sidebar structure: Welcome to MOS + Getting Started

--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,17 @@
-// @ts-check
-
 const sidebars = {
   tutorialSidebar: [
     {
+      type: 'doc',
+      id: 'intro',
+      label: 'Welcome to MOS',
+    },
+    {
       type: 'category',
       label: 'Getting Started',
-      items: ['intro'],
+      items: [
+        // TODO: Füge Dokumente hinzu, sobald sie erstellt sind
+        // { type: 'doc', id: 'Quick-Start/WebUI-Overview', label: 'WebUI Overview' },
+      ],
     },
   ],
 };
