08ef08091be083656a3e7ae6cd2101bb69cb4df5
J000K3R <60364963+J000K3R@users.noreply.github.com>
2026-03-23 11:45:41 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -14,6 +14,7 @@ const sidebars = {
         { type: 'doc', id: 'Quick-Start/Create-Storage-Pool', label: 'Create a Storage Pool' },
         { type: 'doc', id: 'Quick-Start/Start-the-First-Container', label: 'Start the First Container' },
         { type: 'doc', id: 'Quick-Start/Create-the-First-Shares', label: 'Start the First Container' },
+        { type: 'doc', id: 'Quick-Start/Create-the-First-VM', label: 'Start the First Container' },
       ],
     },
   ],
