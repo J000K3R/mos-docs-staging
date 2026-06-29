413a3294c3fc17d0061a228f49c43f4ad6dbfc31
J000K3R <60364963+J000K3R@users.noreply.github.com>
2026-06-29 13:45:18 +0200

    Fix: Hardcode staging redirect path to /mos-docs-staging/docs/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import {Redirect} from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to={useBaseUrl('/docs/')} />;
+  return <Redirect to="/mos-docs-staging/docs/" />;
 }
