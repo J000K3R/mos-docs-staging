8ef187e0a904544a58442b3888aec184e8cea5e4
J000K3R <60364963+J000K3R@users.noreply.github.com>
2026-06-29 13:39:37 +0200

    Fix: Use useBaseUrl for redirect to work with staging baseUrl

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import { Redirect } from '@docusaurus/router';
+import {Redirect} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/docs/" />;
+  return <Redirect to={useBaseUrl('/docs/')} />;
 }
